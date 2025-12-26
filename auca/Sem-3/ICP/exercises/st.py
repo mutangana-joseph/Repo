4388b8090c3467a9b7f9353441576a4053777e9e
mutanganajoseph <josephmutangana65@gmail.com>
2025-12-26 11:47:21 +0200

    update

--- auca/Sem-3/ICP/exercises/st.py
+++ auca/Sem-3/ICP/exercises/st.py
@@ -1,15 +1,14 @@
 
 
-marks=[20,30,40,10,]
+marks=[20,30,40,10,7]
 
 lenght = len(marks)
 
 search = int(input("Enter marks you want to find: "))
 
-for i in range(lenght):
-    if marks[i] != search:
-        print(f"'{search}' not found.")       
-    else:
-        print(f"'{marks[i]}' found at postion {i}") 
-        break
-        
+if search in marks:
+    for i in range(lenght):
+        if search == marks[i]:
+            print(f"{search} found at position '{i}'")
+else:
+    print(f"'{search}' Not found.")
--- auca/Sem-3/ICP/exercises/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-if marks[i] == search:
-        print(f"'{marks[i]}' found at postion {i}")
-    else:
-        print(f"'{search}' not found.")
-        break

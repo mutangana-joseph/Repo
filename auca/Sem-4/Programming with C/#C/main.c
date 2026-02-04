e415c2aa006a8e7f042217d417ed1fdc8f18170a
mutanganajoseph <josephmutangana65@gmail.com>
2026-02-04 21:42:23 +0200

    updates

---
+++ auca/Sem-4/Programming with C/#C/main.c
@@ -0,0 +1,23 @@
+/*First Program */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/*Global Variables*/
+
+int age=10;
+
+/*Functions*/
+
+void display(){
+    printf("Hey %d", age);
+}
+
+
+
+int main(){
+    display();
+    return 0;
+}
+
+
